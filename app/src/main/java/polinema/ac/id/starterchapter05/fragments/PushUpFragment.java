7532ed62e41ee4d2ecb031d73adf92579c74fd10
NuraUrsula <nuraursula980@gmail.com>
2019-10-09 13:18:06 +0700

    menambah fragment pushup

--- app/src/main/java/polinema/ac/id/starterchapter05/fragments/PushUpFragment.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/PushUpFragment.java
@@ -2,12 +2,11 @@ package polinema.ac.id.starterchapter05.fragments;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import polinema.ac.id.starterchapter05.R;
 
@@ -26,7 +25,7 @@ public class PushUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pus_up, container, false);
+        return inflater.inflate(R.layout.fragment_push_up, container, false);
     }
 
 }
